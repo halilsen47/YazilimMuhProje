1372d63ffb25e9e50d4bd9cb4e87c976a8fb37c3
metindeder <metinxdeder@gmail.com>
2026-05-06 16:13:50 +0300

    xunit ve projenin .net sürümü eşitlendi.

--- tests/Business.Tests/UnitTest1.cs
+++ tests/Business.Tests/UnitTest1.cs
@@ -1,10 +1,11 @@
-﻿namespace Business.Tests;
-
-public class UnitTest1
+﻿namespace Business.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
 
+        }
     }
 }
